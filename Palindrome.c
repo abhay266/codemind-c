d3a1de4a1ac277acf8fee6b7dd35890053339d09
abhayoraon <155561514+abhay266@users.noreply.github.com>
2024-01-04 10:18:08 +0530

    #include<stdio.h> int main() {     int n,rev=0,pal,r;     scanf("%d",&n);     pal=n;     while(n!=0)     {         r=n%10;         rev=rev*10+r;         n=n/10;
        }
        if(pal==rev)
        {
            printf("True");
        }
        else{
            printf("False");
        }
    }

---
+++ Palindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,rev=0,pal,r;
+    scanf("%d",&n);
+    pal=n;
+    while(n!=0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+        
+    }
+    if(pal==rev)
+    {
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+}
